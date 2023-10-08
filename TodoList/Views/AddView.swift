ce626ea57b0adc86f56866035a0ff370a75167d6
John Erick Santos <johnsantosdev@gmail.com>
2023-10-08 20:04:21 +1100

    feat: initial setup feat: todo list views setup

--- TodoList/Views/AddView.swift
+++ TodoList/Views/AddView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct AddView: View {
+    @State var textFieldText: String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+                    .padding(.horizontal)
+                    .frame(height: 55)
+                    .background(Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)))
+                    .cornerRadius(10)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Save".uppercased())
+                        .foregroundStyle(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                        .background(Color.accentColor)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                })
+            }
+            .padding(14)
+        }
+        .navigationTitle("Add an Item 🖊️")
     }
 }
 
 #Preview {
-    AddView()
+    NavigationStack {
+        AddView()
+    }
 }
--- TodoList/Views/ListView.swift
+++ TodoList/Views/ListView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct ListView: View {
+    @State var items: [String] = [
+        "This is the first item!",
+        "This is the second",
+        "Third",
+    ]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(items, id: \.self) { item in
+                ListRowView(title: item)
+            }
+        }
+        .listStyle(PlainListStyle())
+        .navigationTitle("Todo List 📝")
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                EditButton()
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                NavigationLink(value: "Add") {
+                    Text("Add")
+                }
+            }
+        }
+        .navigationDestination(for: String.self) { _ in
+            AddView()
+        }
     }
 }
 
 #Preview {
-    ListView()
+    NavigationStack {
+        ListView()
+    }
 }
