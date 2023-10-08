bfca5d22ed8475fd1a9af4838af80804c2586e88
John Erick Santos <johnsantosdev@gmail.com>
2023-10-08 19:35:15 +1100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by John Erick Santos on 8/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
