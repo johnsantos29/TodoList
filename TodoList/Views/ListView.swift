6e521ccab60c4164e0912ee37c5785f738c1ffdf
John Erick Santos <johnsantosdev@gmail.com>
2023-10-08 20:02:37 +1100

    feat: initial setup feat: todo list views setup

---
+++ TodoList/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  TodoList
+//
+//  Created by John Erick Santos on 8/10/2023.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
