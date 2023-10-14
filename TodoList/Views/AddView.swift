4f72d85a603313465b24f008d10e183ec688b744
John Erick Santos <johnsantosdev@gmail.com>
2023-10-14 12:06:32 +1100

    feat: list view model, and environment object

--- TodoList/Views/AddView.swift
+++ TodoList/Views/AddView.swift
@@ -8,7 +8,11 @@
 import SwiftUI
 
 struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText: String = ""
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
 
     var body: some View {
         ScrollView {
@@ -18,13 +22,13 @@ struct AddView: View {
                     .frame(height: 55)
                     .background(Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)))
                     .cornerRadius(10)
-                
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+
+                Button(action: saveButtonPressed, label: {
                     Text("Save".uppercased())
                         .foregroundStyle(.white)
                         .font(.headline)
                         .frame(height: 55)
-                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/ .infinity/*@END_MENU_TOKEN@*/)
                         .background(Color.accentColor)
                         .clipShape(RoundedRectangle(cornerRadius: 10))
                 })
@@ -32,6 +36,28 @@ struct AddView: View {
             .padding(14)
         }
         .navigationTitle("Add an Item 🖊️")
+        .alert(isPresented: $showAlert, content: getAlert)
+    }
+
+    func saveButtonPressed() {
+        if textIsAppropriate() {
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count < 3 {
+            alertTitle = "Note should be greater than 3 characters"
+            showAlert.toggle()
+            return false
+        }
+
+        return true
+    }
+
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
     }
 }
 
@@ -39,4 +65,5 @@ struct AddView: View {
     NavigationStack {
         AddView()
     }
+    .environmentObject(ListViewModel())
 }
